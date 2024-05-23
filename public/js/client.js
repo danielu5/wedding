3b376afae301076c3674afbae2477e193fd4e970
Daniel Molenda <daniel.m.92@wp.pl>
2024-05-23 21:06:58 +0200

    Wyswietlanie listy gości zamiast todo

--- public/js/client.js
+++ public/js/client.js
@@ -16,44 +16,84 @@ const apiFetch = async (url, method, body) => {
     return resp;
 };
 
-const initTodo = () => {
-    // INSERT handleTodoDelete HERE
+const initGuests = () => {
 
-    const renderTitle = ({ title, done }) => {
-        if (!done) return title;
-        return `<s>${title}</s>`;
+    const handleGuestDelete = (ev, id) => {
+        ev.preventDefault();
+        const doDelete = async () => {
+            await apiFetch(`/guests/${id}`, "DELETE");
+            refreshList();
+        }
+        doDelete().catch(err => console.log("Error deleting guest", err));
     };
-    const renderItem = ({ id, title, done }) => `
+    window.handleGuestDelete = handleGuestDelete;
+
+    const renderName = ({ name, present }) => {
+        if (!present) return name;
+        return `<u>${name}</u>`;
+    };
+    const renderAgeGroup = (ageGroup) => {
+        switch (ageGroup) {
+            case 1: {
+                return '0-5';
+            }
+            case 2: {
+                return '5-10';
+            }
+            case 3: {
+                return '>11';
+            }
+            default: {
+                return '~';
+            }
+        }
+    };
+    const renderItem = ({ id, name, present, ageGroup }) => `
         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
+            <div style="flex-grow: 1;">
+                ${renderName({ name, present })}
+                (${renderAgeGroup(ageGroup)})
+            </div>
             <input
                 class="form-check-input me-2"
                 type="checkbox"
                 value=""
-                ${done ? "checked" : ""}
-                onchange="handleTodoChange(event, '${id}')"
+                ${present ? "checked" : ""}
+                onchange="handleGuestChange(event, '${id}')"
             />
-            <div style="flex-grow: 1;">
-                ${renderTitle({ title, done })}
-            </div>
-            <!-- INSERT DELETE BUTTON HERE -->
+            <a
+                style="color: gray; margin: 0 0 0 10px""
+                href="#"
+                role="button"
+                onclick="handleGuestDelete(event, '${id}')"
+                >
+                <i class="far fa-lg fa-trash-alt"></i>
+            </a>
+             <a
+                style="color: gray; margin: 0 0 0 10px"
+                href="/guests/guests.html?id=${id}"
+                role="button"
+                >
+                <i class="far fa-lg fa-edit"></i>
+            </a>
         </li>
     `;
     const noItems = `
         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
-            <strong>No tasks yet. Try adding one.</strong>
+            <span>Jeszcze nie ma żadnych gości</span>
         </li>
     `;
 
     const refreshList = () => {
         const doRefresh = async () => {
-            const list = document.querySelector("#todo-list");
+            const list = document.querySelector("#guest-list");
 
-            const resp = await apiFetch("/todos");
-            const todos = await resp.json();
-            if (todos.length === 0) {
+            const resp = await apiFetch("/guests");
+            const guests = await resp.json();
+            if (guests.length === 0) {
                 list.innerHTML = noItems;
             } else {
-                list.innerHTML = todos.map(renderItem).join("");
+                list.innerHTML = guests.map(renderItem).join("");
             }
         };
 
@@ -62,27 +102,37 @@ const initTodo = () => {
 
     const addItem = async () => {
         const input = document.querySelector("#add-input");
-        const title = input.value;
-        if (!title) return;
+        const name = input.value;
+        if (!name) return;
 
-        await apiFetch("/todos", "POST", { title, done: false });
+        let ageGroup = 3;
+
+        if (document.getElementById('age1').checked == true) {
+            ageGroup = 1;
+        } else if (document.getElementById('age2').checked == true) {
+            ageGroup = 2;
+        } else if (document.getElementById('age3').checked == true) {
+            ageGroup = 3;
+        }
+
+        await apiFetch("/guests", "POST", { name, present: false, ageGroup });
 
         input.value = "";
         refreshList();
     };
 
-    const handleTodoChange = (ev, id) => {
+    const handleGuestChange = (ev, id) => {
         const doChange = async () => {
-            await apiFetch(`/todos/${id}`, "PATCH", { done: ev.target.checked });
+            await apiFetch(`/guests/${id}`, "PATCH", { present: ev.target.checked });
 
             refreshList();
         }
 
-        doChange().catch(err => console.log("Error changing todo done state", err));
+        doChange().catch(err => console.log("Error changing guest done state", err));
     };
-    window.handleTodoChange = handleTodoChange;
+    window.handleGuestChange = handleGuestChange;
 
-    const form = document.querySelector("#todo-form");
+    const form = document.querySelector("#guest-form");
     form.onsubmit = (ev) => {
         ev.preventDefault();
         addItem().catch(err => console.log("Error adding item", err));
@@ -92,4 +142,4 @@ const initTodo = () => {
     refreshList();
 }
 
-initTodo();
+initGuests();
